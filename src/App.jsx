9eeb567178e6c585cdefe8919e5f90a882278fea
Eyinadepeter <itemize6@gmail.com>
2023-11-28 14:50:25 +0100

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Create from "./create";
 
 function App() {
-  <Router>
   return (
+    <Router>
     <div className="App">
       <Navbar />
       <div className="content">
        <Switch>
-        
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route path="/create">
+            <Create/>
+          </Route>
        </Switch>
       </div>
     </div>
-  );
   </Router>
+  );
 }
 
 export default App;
---
+++ src/create.jsx
@@ -0,0 +1,9 @@
+const Create = () => {
+    return (  
+        <div className="create">
+            <h1>Add A New Blog</h1>
+        </div>
+    );
+}
+ 
+export default Create;
