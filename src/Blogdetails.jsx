8056a555b2fe52b67c85719cc31c95627ea342e6
Eyinadepeter <itemize6@gmail.com>
2023-11-30 15:10:20 +0100

    added

--- src/Blogdetails.jsx
+++ src/Blogdetails.jsx
@@ -1,12 +1,22 @@
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+// import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
-    const {id} = useParams()
-    return ( 
-        <div className="blog-details">
-            <h2>Blog Details  - {id}</h2>
-        </div>
-     );
+  const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/');
+
+  return (
+    <div className="blog-details">
+      { isPending && <div>Loading...</div> }
+      { error && <div>{ error }</div> }
+      { blog && (
+        <article>
+          <h2>{ blog.title }</h2>
+          <p>Written by { blog.author }</p>
+          <div>{ blog.body }</div>
+        </article>
+      )}
+    </div>
+  );
 }
  
 export default BlogDetails;
