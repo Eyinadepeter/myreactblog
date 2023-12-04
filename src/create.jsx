3fb68995f3a95ff58eb870da026bbfb5a3d57bae
Eyinadepeter <itemize6@gmail.com>
2023-12-04 16:04:38 +0100

    add

--- src/create.jsx
+++ src/create.jsx
@@ -2,6 +2,17 @@ const Create = () => {
     return (  
         <div className="create">
             <h1>Add A New Blog</h1>
+            <form>
+                <label>Blog Title:</label>
+                <input type="text" required/>
+                <label>Blog Body</label>
+                <input type="textarea" required/>
+                <label> Blog Author:</label>
+                <select name="" id="">
+                    <option value="">yosha</option>
+                    <option></option>
+                </select>
+            </form>
         </div>
     );
 }
